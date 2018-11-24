7c093468eaaa87069ff0328793afc79dc0a097da
sjx233 <sjx233@qq.com>
2018-11-24 19:59:51 +0800

    Do checks in constructor

--- src/index.ts
+++ src/index.ts
@@ -4,15 +4,16 @@ class ResourceLocation {
   public readonly domain: string;
   public readonly path: string;
 
-  public constructor(domain: string, path?: string) {
-    if (path) {
-      this.domain = domain;
-      this.path = path;
-    } else {
-      const indexOfColon = domain.indexOf(":");
-      this.domain = indexOfColon === -1 ? "minecraft" : domain.substring(0, indexOfColon);
-      this.path = domain.substring(indexOfColon + 1);
+  public constructor(domain: string, path?: string, separator = ":") {
+    if (!path) {
+      const indexOfColon = domain.indexOf(separator);
+      path = domain.substring(indexOfColon + 1);
+      domain = indexOfColon === -1 ? "minecraft" : domain.substring(0, indexOfColon);
     }
+    if (!/^[a-z0-9_.-]*$/g.test(domain)) throw new SyntaxError("Invalid resource location: non [a-z0-9_.-] character in domain");
+    if (!/^[a-z0-9/_.-]*$/g.test(domain)) throw new SyntaxError("Invalid resource location: non [a-z0-9/_.-] character in path");
+    this.domain = domain;
+    this.path = path;
   }
 
   public toString() {
