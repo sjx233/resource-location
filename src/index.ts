5a1d8129d81e1f68b1ea2434384f78baa5448c2c
sjx233 <sjx233@qq.com>
2018-12-02 09:16:57 +0800

    Rename `domain` to `namespace`

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,36 @@
 import path from "path";
 
 class ResourceLocation {
-  public readonly domain: string;
+  public readonly namespace: string;
+
+  public get domain() {
+    return this.namespace;
+  }
+
   public readonly path: string;
 
-  public constructor(domain: string, path?: string, separator = ":") {
+  public get name() {
+    return this.path;
+  }
+
+  public constructor(namespace: string, path?: string, separator = ":") {
     if (!path) {
-      const indexOfColon = domain.indexOf(separator);
-      path = domain.substring(indexOfColon + 1);
-      domain = indexOfColon === -1 ? "minecraft" : domain.substring(0, indexOfColon);
+      const indexOfColon = namespace.indexOf(separator);
+      path = namespace.substring(indexOfColon + 1);
+      namespace = indexOfColon === -1 ? "minecraft" : namespace.substring(0, indexOfColon);
     }
-    if (!/^[a-z0-9_.-]*$/g.test(domain)) throw new SyntaxError("Invalid resource location: non [a-z0-9_.-] character in domain");
+    if (!/^[a-z0-9_.-]*$/g.test(namespace)) throw new SyntaxError("Invalid resource location: non [a-z0-9_.-] character in namespace");
     if (!/^[a-z0-9/_.-]*$/g.test(path)) throw new SyntaxError("Invalid resource location: non [a-z0-9/_.-] character in path");
-    this.domain = domain;
+    this.namespace = namespace;
     this.path = path;
   }
 
   public toString() {
-    return this.domain + ":" + this.path;
+    return this.namespace + ":" + this.path;
   }
 
   public toPath(base: string, extension = "") {
-    return path.join(this.domain, base, this.path + extension);
+    return path.join(this.namespace, base, this.path + extension);
   }
 
   public static from(obj: string | ResourceLocation) {
